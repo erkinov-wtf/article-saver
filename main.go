755cbd6e3b37c3e5f8e6d4907aee055a3b647fda
Open Postman <openpostman4oy@gmail.com>
2024-05-09 20:14:38 +0500

    Initial commit - final app

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	tgClient "tgbot/clients/telegram"
+	"tgbot/consumer/event-consumer"
+	"tgbot/events/telegram"
+	"tgbot/storage/sqlite"
+)
+
+const (
+	tgBotHost         = "api.telegram.org"
+	sqliteStoragePath = "data/sqlite/storage.db"
+	batchSize         = 100
+)
+
+func main() {
+	//s := files.New(storagePath)
+	s, err := sqlite.New(sqliteStoragePath)
+	if err != nil {
+		log.Fatal("can't connect to storage: ", err)
+	}
+
+	if err := s.Init(context.TODO()); err != nil {
+		log.Fatal("can't init storage: ", err)
+	}
+
+	eventsProcessor := telegram.New(
+		tgClient.New(tgBotHost, mustToken()),
+		s,
+	)
+
+	log.Print("service started")
+
+	consumer := event_consumer.New(eventsProcessor, eventsProcessor, batchSize)
+
+	if err := consumer.Start(); err != nil {
+		log.Fatal("service is stopped", err)
+	}
+}
+
+func mustToken() string {
+	token := flag.String(
+		"tg-bot-token",
+		"6347212376:AAH72WHstnnXd84DFI3aeZ93sBoLJR2FJ-A",
+		"token for access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
+}
